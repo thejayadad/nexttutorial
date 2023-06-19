6fc764a12d0725e9a4da40cfaa59e576cc7973cb
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-19 19:52:44 -0400

    update

--- next/app/page.js
+++ next/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <section>
-      <h2>Hello World</h2>
+      <h2 className='text-center mt-10'>Hello World</h2>
     </section>
   )
 }
